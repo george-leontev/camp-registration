5148110c4ddbd7cfb673a07a61897dfe0e591d1a
leo <leontev.vyacheslav@gmail.com>
2025-04-26 12:24:39 +0300

    feat: using hql

--- src/main/java/org/church/camp_registration/repository/NotificationRepository.java
+++ src/main/java/org/church/camp_registration/repository/NotificationRepository.java
@@ -2,7 +2,15 @@ package org.church.camp_registration.repository;
 
 import org.church.camp_registration.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface NotificationRepository extends JpaRepository<Notification, Integer>, NotificationRepositoryCustom {
+import java.util.List;
 
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    @Query("""
+                    from Notification as n
+                    where n.telegramId = :telegramId and  n.isDeleted = false
+            """)
+    List<Notification> getAll(@Param("telegramId") String telegramId);
 }
--- src/main/java/org/church/camp_registration/repository/NotificationRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package org.church.camp_registration.repository;
-
-import org.church.camp_registration.model.Notification;
-
-import java.util.List;
-
-public interface NotificationRepositoryCustom {
-    List<Notification> getAll(String telegramId);
-}
